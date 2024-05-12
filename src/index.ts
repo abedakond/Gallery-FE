3563b149965618855c5bcfb808b23686d4bfdc62
Abed <61600847+abedakond@users.noreply.github.com>
2024-05-12 21:18:23 +0100

    image compression 1080 + 480
    - image compression 1080 + 480
    - awaitable upon successful response only

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,56 @@
-import express, { Request, Response} from "express";
+import express, { Request, Response } from "express";
+import { MulterFile } from "./types";
 import multer from 'multer'
-
+import fs from 'fs'
+import path from "path";
+import sharp from "sharp";
+import { rejects } from "assert";
 const app = express();
 const port = 3000;
 
-const upload = multer({ dest: './public/data/uploads/' })
+const destinationPath = './public/data/uploads/'
+
+const storage = multer.diskStorage({
+	destination: function (req: Request, file: MulterFile, cb: Function) {
+		cb(null, destinationPath)
+	},
+	filename: function (req: Request, file: MulterFile, cb: Function) {
+		cb(null, file.originalname)
+	}
+})
+
+const upload = multer({ storage })
 
 app.get('/', (req: Request, res: Response) => {
 	res.send("Hello World")
-})
+});
 
-app.post('/image/upload',upload.single('image'), (req: Request, res: Response) => {
-	console.log(req.file)
-	res.status(200).send()
-})
+app.post('/gallery/upload', upload.single('image'), async (req: Request, res: Response) => {
+	const imagePath = req.file?.path || ''
+	const image = sharp(imagePath);
+	const resizeLow = image.clone().resize(480);
+	const resizeHigh = image.resize(1080)
+	// sharp(imagePath)
+	// 	.resize(480)
+	// 	.toFile('public/result/output_lr.jpg', (err, info) => { console.log(err, info) })
+
+	await Promise.all([
+		new Promise<void>((resolve, reject) => {
+			resizeLow.toFile('public/result/output_lr.jpg', (err, info) => {
+				if (err) reject(err);
+				else resolve();
+			});
+		}),
+		new Promise<void>((resolve, reject) => {
+			resizeHigh.toFile('public/result/output_hr.jpg', (err, info) => {
+				if (err) reject(err);
+				else resolve();
+			});
+		})
+	]);
+	res.json(req.file);
+});
 
 app.listen(port, () => {
 	console.log(`Listening on Port: ${port}`)
-})
+});
