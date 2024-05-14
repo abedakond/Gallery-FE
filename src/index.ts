5850533df88042f4ab1d5f0747eb80dda096e712
Abed <61600847+abedakond@users.noreply.github.com>
2024-05-14 18:04:00 +0100

    update
    update

--- src/index.ts
+++ src/index.ts
@@ -1,102 +1,79 @@
-import express, { Request, Response } from "express";
-import { MulterFile } from "./types";
-import multer from 'multer'
-import fs from 'fs'
-import path from "path";
-import sharp from "sharp";
-import { rejects } from "assert";
+import express, {Request, Response} from 'express';
+import {MulterFile} from './types';
+import multer from 'multer';
+import fs from 'fs';
+import path from 'path';
+import sharp from 'sharp';
+import {rejects} from 'assert';
+import { uploadToSupabase } from './components/supabase-storage/upload-to-supabase';
 
 const app = express();
 const port = 3000;
 
-const FILE_TYPE_MAP: { [key: string]: string } = {
-	'image/png': 'png',
-	'image/jpeg': 'jpeg',
-	'image/jpg': 'jpg',
+const FILE_TYPE_MAP: {[key: string]: string} = {
+  'image/png': 'png',
+  'image/jpeg': 'jpeg',
+  'image/jpg': 'jpg',
 };
 
-const destinationPath = './public/uploads/'
-const compressedImgPath  = './public/compressed/'
+const destinationPath = './public/uploads/';
+const compressedImgPath = './public/compressed/';
 
 // Ensure that the compressed image directory exists
-fs.mkdirSync(compressedImgPath, { recursive: true });
+fs.mkdirSync(compressedImgPath, {recursive: true});
 
 const storage = multer.diskStorage({
-	destination: function (req: Request, file: MulterFile, cb: Function) {
-		const isValid = FILE_TYPE_MAP[file.mimetype]
-		if(isValid) {
-			cb(null, destinationPath);
-		} else {
-			const uploadError = new Error('invalid image type'); 
-			cb(uploadError, destinationPath);
-		} 
-	},
-	filename: function (req: Request, file: MulterFile, cb: Function) {
-		const extension = FILE_TYPE_MAP[file.mimetype];
-		cb(null, `${file.originalname}-${Date.now()}.${extension}`);
-	}
-})
+  destination: function (req: Request, file: MulterFile, cb: Function) {
+    const isValid = FILE_TYPE_MAP[file.mimetype];
+    if (isValid) {
+      cb(null, destinationPath);
+    } else {
+      const uploadError = new Error('invalid image type');
+      cb(uploadError, destinationPath);
+    }
+  },
+  filename: function (req: Request, file: MulterFile, cb: Function) {
+    const extension = FILE_TYPE_MAP[file.mimetype];
+    cb(null, `${file.originalname}-${Date.now()}.${extension}`);
+  },
+});
 
-const upload = multer({ storage })
+const upload = multer({storage});
 
 app.get('/', (req: Request, res: Response) => {
-	res.send("Hello World")
+  res.send('Hello World');
 });
 
-app.post('/gallery/upload', upload.single('image'), async (req: Request, res: Response) => {
-	// const imagePath = req.file?.path || ''
-	// const image = sharp(imagePath);
-	// const resizeLow = image.clone().resize(480);
-	// const resizeHigh = image.resize(1080);
+app.post(
+  '/gallery/upload',
+  upload.single('image'),
+  async (req: Request, res: Response) => {
+    const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
+    const fileName = req.file?.filename;
 
-	const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
-	const fileName = req.file?.filename
-
-	console.log(basePath)
-	console.log(fileName)
-
-
-	// Create low-resolution image
     const lowResImgPath = `${compressedImgPath}${fileName}_lr.jpg`;
-    const lowResImgUrl = `${basePath}${fileName}_lr.jpg`;
-
-	console.log("Low res image path:", lowResImgPath);
-	console.log("Low res image url:", lowResImgUrl);
-
-    await sharp(req.file?.path || '').resize(480).toFile(lowResImgPath);
-
-    // Create high-resolution image
     const highResImgPath = `${compressedImgPath}${fileName}_hr.jpg`;
-    const highResImgUrl = `${basePath}${fileName}_hr.jpg`;
 
-	console.log("High res image path:", highResImgPath);
-	console.log("High res image url:", highResImgUrl);
+    console.log('Low res image path:', lowResImgPath);
+    console.log('High res image path:', highResImgPath);
 
-    await sharp(req.file?.path || '').resize(1080).toFile(highResImgPath);
-	// sharp(imagePath)
-	// 	.resize(480)
-	// 	.toFile('public/result/output_lr.jpg', (err, info) => { console.log(err, info) })
+    await sharp(req.file?.path || '')
+      .resize(480)
+      .toFile(lowResImgPath);
 
-	// await Promise.all([
-	// 	new Promise<void>((resolve, reject) => {
-	// 		resizeLow.toFile(`${lowResImgPath}`, (err, info) => {
-	// 			if (err) reject(err);
-	// 			else resolve();
-	// 		});
-	// 	}),
-	// 	new Promise<void>((resolve, reject) => {
-	// 		resizeHigh.toFile(`${highResImgPath}`, (err, info) => {
-	// 			if (err) reject(err);
-	// 			else resolve();
-	// 		});
-	// 	})
-	// ]);
-	
-	//res.json(req.file);
+    await sharp(req.file?.path || '')
+      .resize(1080)
+      .toFile(highResImgPath);
+		
+		const lowResFile = fs.readFileSync(lowResImgPath )
+		const highResFile = fs.readFileSync(highResImgPath)
 
-	res.json({ lowResImgUrl, highResImgUrl });
-});
+    await uploadToSupabase(lowResFile, lowResImgPath, highResFile, highResImgPath);
+
+    res.json({lowResImgPath, highResImgPath});
+  }
+);
 
 app.listen(port, () => {
-	console.log(`Listening on Port: ${port}`)
+  console.log(`Listening on Port: ${port}`);
 });
